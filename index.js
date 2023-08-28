edbe5079162b8bc7e7c0d3e7584070c99e1a5e63
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-08-28 19:35:27 +0530

    handling patch requests

--- index.js
+++ index.js
@@ -20,9 +20,30 @@ app.post("/api/tours", (req, res) => {
     "./dev-data/data/tours-simple.json",
     JSON.stringify(tours),
     (err) => {
-      res.status(200).json({ message: "success" });
+      res.status(200).json({ message: "success", data: { tour: newTour } });
     }
   );
+});
+
+app.get("/api/tours/:id", async (req, res) => {
+  const id = req.params.id * 1;
+
+  try {
+    if (id > tours.length) {
+      return res.status(404).send("Tour not found");
+    }
+    const tour = tours.find((el) => el.id === id);
+    res.status(200).json({ message: "success", tours: { tour } });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.patch("/api/tours/:id", async (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).send("Tour not found");
+  }
+  res.status(200).json({ message: "success", tour: "Updated Tour" });
 });
 
 app.listen(8080, () => {
